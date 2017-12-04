c0e86912afe44b49429bb509c5fba4edb6b1f12c
kandaness <kandaness@163.com>
2017-12-04 23:04:09 +0800

    分割线组件和路由设置

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+import router from './router/index.js';
+import VueResource from 'vue-resource';
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl';
 
+Vue.use(VueResource);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: {
-    App
-  }
-})
+  render:h=>h(App)
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
 
-Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+Vue.use(Router);
+
+const routes=[{
+    path:'/',
+    redirect:'/goods'
+  },{
+    path:'/goods',
+    components:goods
+  },{
+    path:'/ratings',
+    components:ratings
+  },{
+    path:'/seller',
+    components:seller
+  }];
+
+  export default new Router({
+    linkActiveClass:'active',
+    routes
+  });
